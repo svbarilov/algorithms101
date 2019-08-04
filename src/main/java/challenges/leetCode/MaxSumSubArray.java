442178eb391964c1077a577dc908d57f5510815b
Sergei <sbarylau@salesforce.com>
2019-08-04 08:33:28 -0700

    Adds MaxProductSubArray Problem

---
+++ src/main/java/challenges/leetCode/MaxSumSubArray.java
@@ -0,0 +1,21 @@
+package challenges.leetCode;
+
+public class MaxSumSubArray {
+
+    //    public int maxProduct(int[] nums) {
+//
+//        int n = nums.length;
+//        int maximumSubArraySum = Integer.MIN_VALUE;
+//
+//        for (int left = 0; left < n; left++) {
+//            int runningWindowSum = 0;
+//            for (int right = left; right < n; right++) {
+//                runningWindowSum *= nums[right];
+//                maximumSubArraySum = Math.max(maximumSubArraySum, runningWindowSum);
+//            }
+//        }
+//        return maximumSubArraySum;
+//    }
+
+    
+}
---
+++ src/main/java/challenges/leetCode/MaximumProductSubarray.java
@@ -0,0 +1,62 @@
+package challenges.leetCode;
+
+public class MaximumProductSubarray {
+//    Input: [2,3,-2,4]
+//    Output: 6
+//    Explanation: [2,3] has the largest product 6.
+
+    public static void main(String args[]) {
+        int[] nums = {2, 3, -2, 4};
+//        int[] nums = {0, 2};
+//        int[] nums = {3, -1, 4};
+//        int[] nums = {-4, -3, -2};
+//        int[] nums = {-2, 3, -4};
+        int result = maxProduct(nums);
+        System.out.println(result);
+    }
+
+//    public static int maxProduct(int[] nums) {
+//        if (nums.length == 0) {
+//            return 0;
+//        } else if (nums.length == 1) {
+//            return nums[0];
+//        }
+//        int curMax = Integer.MIN_VALUE;
+//        for (int i = 0; i < nums.length; i++ ) {
+//            int itMax = nums[i];
+//            int sumSoFar = 1;
+//            for (int j = i; j < nums.length; j++) {
+//                sumSoFar *= nums[j];
+//                itMax = Math.max(itMax, sumSoFar);
+//            }
+//            curMax = Math.max(curMax, itMax);
+//        }
+//        return curMax;
+//    }
+
+    public static int maxProduct(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        } else if (nums.length == 1) {
+            return nums[0];
+        }
+        int max = nums[0];
+        int min = nums[0];
+        int itMin = nums[0];
+        int itMax = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            int tempMax;
+            tempMax = Math.max(Math.max(nums[i], itMax*nums[i]), Math.max(nums[i], itMin*nums[i]));
+            itMin = Math.min(Math.min(nums[i], itMax*nums[i]), Math.min(nums[i], itMin*nums[i]));
+            itMax = tempMax;
+            max = Math.max(itMax, max);
+            min = Math.min(itMin, min);
+        }
+        return max;
+    }
+
+
+
+}
+
+
